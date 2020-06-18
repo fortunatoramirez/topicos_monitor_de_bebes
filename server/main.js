843a101e827d3d350d6fc7a315b8e5e58420afaf
Fortunato <fortunato.ramirez@tectijuana.edu.mx>
2020-06-18 15:41:14 -0700

    agragando la carpeta private 14

--- server/main.js
+++ server/main.js
@@ -24,7 +24,7 @@ con.connect(function(err) {
 
 app.use(express.static('public'));
 
-app.use('/home', express.static('private');
+app.use('/home', express.static('/private'));
 
 app.use(session({
 	secret: 'secret',
