f0546df16eacf1f16f8b243cfd1e0342e280f956
Fortunato <fortunato.ramirez@tectijuana.edu.mx>
2020-06-23 15:26:47 -0700

    main2 1

---
+++ server/main2.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var io = require('socket.io')(server);
+var path = require('path');
+var fs = require('fs');
+
+/* Conexión a la base de datos */
+var mysql = require('mysql');
+var con = mysql.createConnection({
+	host: "localhost",
+	user: "topicos",
+	password: "tec123", //colocar aquí la contraseña de su base de datos
+	database: "topicos"
+
+});
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected to database.");
+});
+/********************************/
+
+
+app.use("/",express.static(__dirname+'public'));
+
+
+
+server.listen(5001, function(){
+	console.log("Servidor corriendo en http://localhost:5001");
+
+});
